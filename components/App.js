d1fce3932ec199afaf57f0dcdfd5668cf6a88d1a
Daniel Shin <shin.daniel.d@gmail.com>
2017-04-02 17:20:25 -0600

    final touches

--- components/App.js
+++ components/App.js
@@ -2,3 +2,13 @@ import React from 'react'
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibileTodoList from '../containers/VisibleTodoList'
+
+const App = () => {
+  <div>
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
+  </div>
+}
+
+export default App
